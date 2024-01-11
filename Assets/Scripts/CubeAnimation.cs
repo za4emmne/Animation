33265d3df857ffe450e5a64f9140dd0d543b37c1
ILYA MALTSEV <iluhaha@mail.ru>
2024-01-11 23:19:36 +0500

    Update CubeAnimation.cs

--- Assets/Scripts/CubeAnimation.cs
+++ Assets/Scripts/CubeAnimation.cs
@@ -6,7 +6,6 @@ public class CubeAnimation : MonoBehaviour
 {
     [SerializeField] private float _speed;
 
-    // Update is called once per frame
     private void Update()
     {
         transform.Rotate(0, _speed, 0);
